d7523c1f7b507bb9d76d2b022926a6a00b4b302f
eightgran <eightgran@gmail.com>
2021-08-08 17:58:55 +0200

    Implement an `IconButton` component

---
+++ src/components/iconbutton.js
@@ -0,0 +1,24 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+export default function IconButton(props) {
+  return (
+    <button
+      onClick={() => {}}
+      className={
+        "bg-white hover:bg-gray-400 text-gray-600 hover:text-white " +
+        " text-xs font-bold uppercase px-5 py-2 rounded-full shadow hover:shadow-md hover:text-white outline-none focus:outline-none lg:mr-1 lg:mb-0"
+      }
+      type="button"
+      style={{ transition: "all .75s ease" }}
+    >
+      <span>
+        <span className="pr-2">
+          <FontAwesomeIcon icon={props.icon} />
+        </span>
+        <span className="font-semibold tracking-wider">{props.title}</span>
+      </span>
+    </button>
+  );
+}
