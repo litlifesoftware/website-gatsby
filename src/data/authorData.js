28b3878541811907908b25e268f8083ce81fa9cd
eightgran <eightgran@gmail.com>
2021-08-12 21:19:06 +0200

    Add `authorData`

---
+++ src/data/authorData.js
@@ -0,0 +1,10 @@
+import Icon256 from "../components/icons/icon_256";
+
+const authorData = {
+  name: "LitLifeSoftware",
+  slogan: "choose a lit life",
+  description:
+    "LitLifeSoftware is an independent creator of mobile apps. All apps featured on this website are free to use and open source. We don't collect data and neither intent to do so in the future. Our goal is to provide offline-first and privacy oriented experiences.",
+  icon: Icon256,
+};
+export default authorData;
