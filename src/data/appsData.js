c688610e1ded6fce594f6f44e157d4f9c721c015
eightgran <eightgran@gmail.com>
2021-08-12 21:19:22 +0200

    Add `appsData`

---
+++ src/data/appsData.js
@@ -0,0 +1,21 @@
+import HistoryOfMeIcon from "../components/icons/history_of_me_icon";
+import RemainingLifetimeIcon from "../components/icons/remaining_lifetime_icon";
+
+
+const appsData = [
+  {
+    title: "History of Me",
+    slogan: "Your personal digital diary.",
+    description:
+      "History of Me allows you to keep a private and digital diary on your phone.",
+    icon: HistoryOfMeIcon,
+  },
+  {
+    title: "Remaining Lifetime",
+    slogan: "Keep track of your life achievements.",
+    description: "Remaining Lifetime allows you to visualize your entire life and motivates you to make every month count.",
+    icon: RemainingLifetimeIcon,
+  },
+];
+
+export default appsData;
