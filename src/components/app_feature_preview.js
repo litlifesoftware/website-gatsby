8ce0e8c96ace8d0b30dae5ebcd0b5082d769d8ac
eightgran <eightgran@gmail.com>
2021-08-12 21:26:15 +0200

    Add `AppFeaturePreview` component

---
+++ src/components/app_feature_preview.js
@@ -0,0 +1,42 @@
+import { faGooglePlay } from "@fortawesome/free-brands-svg-icons";
+import * as React from "react";
+import IconButton from "./iconbutton";
+
+const AppFeaturePreview = (props) => {
+  return (
+    <div className="container mx-auto px-4 xl:w-1/2">
+      <div className="items-center flex flex-wrap">
+        <div className="w-full md:w-3/12 mx-auto px-4 align-top">
+          <div
+            className="flex rounded-4xl mx-auto sm:mx-0 bg-white shadow-lg w-48 h-48"
+            data-aos="fade-right"
+          >
+            {
+              <props.icon className="w-28 h-28 mx-auto my-auto py-auto px-auto" />
+            }
+          </div>
+        </div>
+
+        <div
+          className="w-full md:w-9/12 mx-auto px-4 sm:px-8 align-top"
+          data-aos="fade-left"
+        >
+          <div className="py-4 px-4">
+            <h3 className="text-2xl font-semibold  text-gray-800 text-center sm:text-left">
+              {props.title}
+            </h3>
+            <p className="my-4 text-lg leading-relaxed text-gray-600 text-center sm:text-left">
+              {props.slogan}
+            </p>
+            <p>{props.description}</p>
+            <div className="mt-8 text-center sm:text-left">
+              <IconButton title="DOWNLOAD" icon={faGooglePlay} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppFeaturePreview;
