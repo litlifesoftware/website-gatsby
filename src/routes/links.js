13176b749e44f1d10d2adbab9c0dd14c6fdf6280
eightgran <eightgran@gmail.com>
2021-08-13 19:19:28 +0200

    Add external `links` data

---
+++ src/routes/links.js
@@ -0,0 +1,37 @@
+import {
+  faGithub,
+  faGooglePlay,
+  faInstagram,
+  faReddit,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+const links = [
+  {
+    title: "GitHub",
+    url: "https://github.com/litlifesoftware",
+    icon: faGithub,
+  },
+  {
+    title: "Google Play",
+    url: "https://play.google.com/store/apps/dev?id=6358212632242842077",
+    icon: faGooglePlay,
+  },
+  {
+    title: "Instagram",
+    url: "https://www.instagram.com/litlifesw/",
+    icon: faInstagram,
+  },
+  {
+    title: "Reddit",
+    url: "https://www.reddit.com/user/litlifesoftware/",
+    icon: faReddit,
+  },
+  {
+    title: "YouTube",
+    url: "https://www.youtube.com/channel/UC0r6RzrH6K1yTQoVYDfxVZg/",
+    icon: faYoutube,
+  },
+];
+
+export default links;
