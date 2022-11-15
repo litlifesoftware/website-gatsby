c70d3cc42c436840ac22d62c7c826156723d33fb
eightgran <eightgran@gmail.com>
2022-11-15 15:07:58 +0100

    Update `index` page - Add new showcase section - Update branding on hero

---
+++ src/components/showcase.js
@@ -0,0 +1,42 @@
+import React from "react";
+import authorData from "../data/author_data";
+import IconButton from "./iconbutton";
+import { faGooglePlay } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "gatsby";
+
+export default function Showcase(props) {
+  return (
+    <div className="max-w-7xl mx-auto py-12 px-8 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <div>
+        <h6 className="text-2xl lg:text-3xl leading-9 font-bold tracking-widest text-customPurple-dark sm:leading-10">
+          {props.title.toUpperCase()}
+        </h6>
+        <p className="text-customRed-600 font-normal text-xl pt-4">
+          <span>{props.subtitle}</span>
+        </p>
+      </div>
+      <div className="py-4">
+        <Link to={props.link}>
+          <button
+            className={
+              "text-gray-600" +
+              " " +
+              "hover:bg-gray-400 hover:text-white" +
+              " " +
+              "font-bold uppercase px-8 py-2 my-4 sm:2 rounded-full shadow hover:shadow-md hover:text-white outline-none focus:outline-none lg:mr-1 lg:mb-0" +
+              " " +
+              "text-base" +
+              " " +
+              "w-full sm:w-auto"
+            }
+            type="button"
+          >
+            <span>
+              <span className="font-bold tracking-widest">{props.title}</span>
+            </span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,47 +5,65 @@ import Icon256 from "../components/icons/icon_256";
 import authorData from "../data/author_data";
 import productData from "../data/product_data";
 import AppFeaturePreview from "../components/app_feature_preview";
+import Badge960 from "../components/icons/badge_960";
+import Showcase from "../components/showcase";
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="LitLifeSoftware" />
+    <Seo title="Home" />
 
     <div className="h-screen w-full bg-gradient-to-bl from-herogradient-100 to-herogradient-200">
       <div className="container mx-auto">
         <div className="flex items-center justify-center h-screen w-full backdrop-blur-sm">
           <div>
-            <Icon256 className="mx-auto py-8" data-aos="fade-right" />
-            <div
+            <Badge960
+              className="mx-auto py-8 h-40 md:h-52 lg:h-64 xl:h-80  text-customRed-logo fill-current"
+              data-aos="fade-right"
+            />
+            {/* <div
               data-aos="fade-up"
               className="text-center font-extrabold text-4xl tracking-wide text-gray-200"
             >
               {authorData.slogan}
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
     </div>
 
-    <section className="py-12 bg-customGray-100">
+    <section className="py-12 bg-customGray-300">
       <div className="container mx-auto px-4 xl:w-1/2">
         <div className="flex flex-wrap">
           <div className="w-full px-auto text-left" data-aos="fade-up">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-2xl">
-              <div className="px-6 py-5 flex-auto">
-                <h6 className="text-3xl font-semibold pb-4 text-gray-300">
-                  About
-                </h6>
-                <p className="text-gray-600 font-light tracking-wider">
-                  {authorData.description}
-                </p>
-              </div>
+            <div className="px-6 py-5 flex-auto break-words">
+              <h6 className="text-3xl font-bold pb-4 text-gray-500 tracking-widest">
+                {"About".toUpperCase()}
+              </h6>
+              <p className="text-gray-600 font-normal tracking-wider">
+                {authorData.description}
+              </p>
             </div>
           </div>
         </div>
       </div>
     </section>
 
-    <section className="relative py-20">
+    <section className="relative py-4">
+      <Showcase
+        title="Apps"
+        subtitle="Check out our privacy-focused apps."
+        link={"/apps"}
+      />
+            <Showcase
+        title="Packages"
+        subtitle="Build your own using our Flutter Packages."
+        link={"/packages"}
+      />
+    </section>
+
+
+
+    {/* <section className="relative py-20">
       {productData.map((item, key) => {
         return (
           <AppFeaturePreview
@@ -56,10 +74,11 @@ const IndexPage = () => (
             description={item.description}
             url={item.url}
             route={item.route}
+            github={item.github}
           />
         );
       })}
-    </section>
+    </section> */}
   </Layout>
 );
 
