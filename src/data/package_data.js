f5c91adadfd11e91618698066d50f7dca2edb6cc
eightgran <eightgran@gmail.com>
2022-11-15 14:56:02 +0100

    Add package data - Contains meta data about released packages

---
+++ src/data/package_data.js
@@ -0,0 +1,44 @@
+const packageData = [
+  {
+    title: "Leitmotif",
+    slogan: "Flutter Package",
+    description:
+      "A collection of widgets and screens for Flutter implementing the Leitmotif Design Language.",
+    github: "https://github.com/litlifesoftware/leitmotif-flutter",
+    pubdev: "https://pub.dev/packages/leitmotif",
+    latestRelease:
+      "https://github.com/litlifesoftware/leitmotif-flutter/releases/latest",
+  },
+  {
+    title: "Date Colors",
+    slogan: "Flutter Package",
+    description:
+      "A Flutter package to generate a color scheme based on a date. The color scheme includes a color for the calendar day and a color for the year.",
+    github: "https://github.com/litlifesoftware/date_colors",
+    pubdev: "https://pub.dev/packages/date_colors",
+    latestRelease:
+      "https://github.com/litlifesoftware/date_colors/releases/latest",
+  },
+  {
+    title: "Lit Backup Service",
+    slogan: "Flutter Package",
+    description:
+      "A Flutter package to create and restore backups using JSON files.",
+    github: "https://github.com/litlifesoftware/lit_backup_service",
+    pubdev: "https://pub.dev/packages/lit_backup_service",
+    latestRelease:
+      "https://github.com/litlifesoftware/lit_backup_service/releases/latest",
+  },
+  {
+    title: "Lit Relative DateTime",
+    slogan: "Flutter Package",
+    description:
+      "A Flutter package to generate relative dates to show differences in time in localized and human-readable format.",
+    github: "https://github.com/litlifesoftware/lit_relative_date_time",
+    pubdev: "https://pub.dev/packages/lit_relative_date_time",
+    latestRelease:
+      "https://github.com/litlifesoftware/lit_relative_date_time/releases/latest",
+  },
+];
+
+export default packageData;
