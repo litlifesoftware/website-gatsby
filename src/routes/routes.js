827e3c35e84c4e4f100b02feca1920a4a42d1e88
eightgran <eightgran@gmail.com>
2022-11-15 15:05:27 +0100

    Update `routes` - Add new pages - Remove deprecated pages

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,24 +7,35 @@ const routes = [
     component: IndexPage,
     showOnNavbar: true,
     showOnFooter: false,
-  },
-  {
-    path: "/remaininglifetime",
-    title: "Remaining Lifetime",
-    showOnNavbar: true,
-    showOnFooter: false,
+    isLegal: false,
   },
   {
     path: "/historyofme",
     title: "History of Me",
-    showOnNavbar: true,
+    showOnNavbar: false,
     showOnFooter: false,
+    isLegal: false,
+  },
+  {
+    path: "/apps",
+    title: "Apps",
+    showOnNavbar: true,
+    showOnFooter: true,
+    isLegal: false,
+  },
+  {
+    path: "/packages",
+    title: "Packages",
+    showOnNavbar: false,
+    showOnFooter: true,
+    isLegal: false,
   },
   {
     path: "/privacy",
     title: "Privacy",
     showOnNavbar: false,
     showOnFooter: true,
+    isLegal: true,
   },
 ];
 
