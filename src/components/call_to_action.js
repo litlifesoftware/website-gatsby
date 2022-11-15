78ecb57bc2da0bac78390b334c9a0d36d5295ea2
eightgran <eightgran@gmail.com>
2022-11-15 15:04:33 +0100

    Update styling on `CallToAction` component - Alter colors - Alter font

--- src/components/call_to_action.js
+++ src/components/call_to_action.js
@@ -8,8 +8,8 @@ export default function CallToAction(props) {
     <div className="bg-customRed-100">
       <div className="max-w-7xl mx-auto py-12 px-8 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <div>
-          <h6 className="text-4xl leading-9 font-extrabold tracking-tight text-customRed-500 sm:leading-10">
-            Want to try out our apps?
+          <h6 className="text-2xl lg:text-3xl leading-9 font-bold tracking-widest text-customPurple-dark sm:leading-10">
+            {"Want to try out our apps?".toUpperCase()}
           </h6>
           <p className="text-customRed-600 font-normal text-xl pt-4">
             <span>Download them now on Google Play.</span>
